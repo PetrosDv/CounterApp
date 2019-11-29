a63f3b26e083826346d6d3ba077b7da426575f9c
Petros Dv <petrossdv@gmail.com>
2019-11-29 21:15:17 +0100

    Handling Events

--- counter-app/src/components/Counter.jsx
+++ counter-app/src/components/Counter.jsx
@@ -12,24 +12,38 @@ class Counter extends Component {
   //   fontWeight: "bold"
   // };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}> {tag} </li>
-        ))}
-      </ul>
-    );
+  handleIncrement() {
+    console.log("Increment clicked");
   }
 
   render() {
-    return;
-    <div>
-      {this.state.tags.length === 0 && "Please add some tags!"}
-      {this.renderTags()}
-    </div>;
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onclick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}> {tag} </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
