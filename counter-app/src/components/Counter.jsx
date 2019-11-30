5e5a63e0369219bff38affb13d252fdeab2034ad
Petros Dv <petrossdv@gmail.com>
2019-11-30 22:08:45 +0100

    Single Source of Truth / Remove the local state

--- counter-app/src/components/Counter.jsx
+++ counter-app/src/components/Counter.jsx
@@ -1,18 +1,7 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value
-    //tags: ["tag1", "tag2", "tag3"] // I will use the map method to render list of items
-    //imageUrl: "https://picsum.photos/199" // <img src={this.state.imageUrl} alt="" />
-  };
-
   //error function syntax: function = (..) => {...}
-  handleIncrement = product => {
-    console.log(product);
-    //this.state.count++; <-- this is not working in React, instead we use below
-    this.setState({ value: this.state.value + 1 });
-  };
 
   render() {
     //it returns a new react element - div - which has 2 children (span & button)
@@ -20,7 +9,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onclick={() => this.handleIncrement(product)}
+          onclick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -37,13 +26,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
