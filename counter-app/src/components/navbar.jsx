70792750d15a6f938237387d895b5c00af72a435
Petros Dv <petrossdv@gmail.com>
2019-12-01 17:59:29 +0100

    Stateless Functional Components

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+//We can use instead of a class a function "Stateless Function Components"
+//Insted of having a class extends component with a render method, we define a function
+//that returns a React element
+//*we use sfc - tab - to create a stateless functional component.
+
+const NavBar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
